6650fd94e3e7d104c4e129b8155f6475bfa4f35a
Oleg Naumov <oleg.naumov.98@mail.ru>
2019-05-11 15:23:58 +0300

    start hyperskill project

---
+++ hyperskill/src/linalg/Main.java
@@ -0,0 +1,9 @@
+package linalg;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Linear Equation Solver");
+        System.out.println(5+5);
+    }
+}
